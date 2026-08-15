d8b6fcd769323fd5f85fa9d4d09cd2d80d5a6646
Sanyam <sanyamg2018@gmail.com>
2026-08-15 13:34:09 +0530

    Implemented React Router for setup

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
 
 function App() {
 
   return (
     <>
-    <h1>Hello world</h1>
+      <RouterProvider router={routes} />
     </>
   )
 }
